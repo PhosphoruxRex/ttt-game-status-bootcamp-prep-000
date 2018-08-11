3d3c48b23f2ae4679de131e0234ddb2e6f9a0d2a
Lung Chen <lungheichen@gmail.com>
2018-08-11 22:02:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,9 +19,7 @@ def won?(board)
   players = ["X", "O"]
   players.each do |player|
     WIN_COMBINATIONS.any? do |combo|
-      if (board[combo[0]] == player)
-      && (board[combo[1]] == player)
-      && (board[combo[2]] == player)
+      if (board[combo[0]] == player) && (board[combo[1]] == player) && (board[combo[2]] == player)
         return combo
       end
     end
